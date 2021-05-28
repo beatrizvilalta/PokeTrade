772d77ca0b5c74e78badfd56902f771a7df81358
Beatriz Vilalta <beatrizvilaltajimenez@gmail.com>
2021-05-27 21:41:15 -0300

    fixed layout

--- FrontEnd/src/views/Home.vue
+++ FrontEnd/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/pokemon-logo.png">
+    <img alt="PokeTrade" src="../assets/pokemon-logo.png">
     <h1  class="title is-2">Pokemon Trade</h1>
     <h4 class="title is-3">Choose your decks and see if it is worth the trade</h4>
     <router-link to="/trades" tag="button" class="button is-rounded" id="startBtn">Start Trade</router-link>
