985d41414c7b9a3e2360d7bc9a9a8ddc69da3a7d
Beatriz Vilalta <beatrizvilaltajimenez@gmail.com>
2021-06-03 11:05:46 -0300

    Added pg config

---
+++ api/controllers/dbController.js
@@ -0,0 +1,16 @@
+module.exports = app => {
+    const controller = {};
+
+    controller.listDb = (async (req, res) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query('SELECT * FROM test_table');
+            const results = { 'results': (result) ? result.rows : null };
+            res.render('pages/db', results);
+            client.release();
+        } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+        }
+    });
+}
