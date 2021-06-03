a2a27ebc0f2fab65840ce6caa1b89c0b92545b06
Beatriz Vilalta <beatrizvilaltajimenez@gmail.com>
2021-06-02 21:47:38 -0300

    added production variable

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const dbEngine = process.env.DB_ENVIRONMENT || 'development';
 const config = require('config')[dbEngine];
 
-const connection = new Sequelize('poketrade', config.get('DB_USERNAME'), config.get('DB_PASSWORD'), {
+const connection = new Sequelize(config.get('DB_NAME'), config.get('DB_USERNAME'), config.get('DB_PASSWORD'), {
   host: config.get('DB_HOST'),
   dialect: 'postgres'
 });
