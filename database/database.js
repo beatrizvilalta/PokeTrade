06a09afd80000bb3a478b66637de2c7d93a77618
Beatriz Vilalta <beatrizvilaltajimenez@gmail.com>
2021-06-03 11:48:22 -0300

    Fixed bugs

--- database/database.js
+++ database/database.js
@@ -5,14 +5,14 @@ const config = require('config')[dbEngine];
 
 const connection = new Sequelize(config.get('DB_NAME'), config.get('DB_USERNAME'), config.get('DB_PASSWORD'), {
   host: config.get('DB_HOST'),
-  dialect: 'postgres'
-  // protocol: "postgres",
-  // dialectOptions: {
-  //     ssl: {
-  //         require: true,
-  //         rejectUnauthorized: false
-  //     }
-    
+  dialect: 'postgres',
+  operatorsAliases: false,
+  pool: {
+    max: 5,
+    min:  0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./config/express')();
 const port = app.get('port');
 const connection = require("./database/database");
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 
 //Database connection
 connection
@@ -18,22 +18,22 @@ app.listen(port, () => {
 });
 
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
 
-app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.sen('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
+// app.get('/db', async (req, res) => {
+//     try {
+//       const client = await pool.connect();
+//       const result = await client.query('SELECT * FROM test_table');
+//       const results = { 'results': (result) ? result.rows : null};
+//       res.send('pages/db', results );
+//       client.release();
+//     } catch (err) {
+//       console.error(err);
+//       res.send("Error " + err);
+//     }
+//   })
