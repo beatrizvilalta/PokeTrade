e2dd2327cc0afd29bfaa3f3dbf250f3af6a42b1b
Beatriz Vilalta <beatrizvilaltajimenez@gmail.com>
2021-06-02 22:04:37 -0300

    added catch

--- database/database.js
+++ database/database.js
@@ -6,6 +6,6 @@ const config = require('config')[dbEngine];
 const connection = new Sequelize(config.get('DB_NAME'), config.get('DB_USERNAME'), config.get('DB_PASSWORD'), {
   host: config.get('DB_HOST'),
   dialect: 'postgres'
-}).then(console.log("connected")).catch(console.error);
+}).then(console.log("connected"));
 
 module.exports = connection;
