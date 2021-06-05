9682775dc35ab115a7dcd1197328aaa223c88d0c
Beatriz Vilalta <beatrizvilaltajimenez@gmail.com>
2021-06-05 11:11:16 -0300

    Fixed production as environment

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const dbEngine = process.env.DB_ENVIRONMENT || 'development';
-const config = require('config')['production'];
+const dbEngine = process.env.DB_ENVIRONMENT;
+const config = require('config')[dbEngine];
 
 const connection = new Sequelize(config.get('DB_NAME'), config.get('DB_USERNAME'), config.get('DB_PASSWORD'), {
   host: config.get('DB_HOST'),
